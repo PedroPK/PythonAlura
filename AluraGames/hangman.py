53715ec1534fc392e3fed521f00831689581c02e
pedropk <pedropk@gmail.com>
2020-06-04 19:53:06 -0300

    Python 3 - Part 02 - Class 02.01

--- AluraGames/hangman.py
+++ AluraGames/hangman.py
@@ -2,6 +2,27 @@ def play():
     print("#" * 30)
     print("Welcome to Hangman Game")
     print("#" * 30)
+    print("\n")
+
+    secretWord = "banana"
+
+    isHanged        = False
+    gotWord         = False 
+
+    while ( not isHanged and not gotWord ) :
+        guess   =   input("Type a character: ")
+        gotCharacter    = False
+
+        index = 0
+        for character in secretWord :
+            if ( character == guess) :
+                print("You got one. The character {} exists in position {}".format(character, index))
+                gotCharacter = True
+                break
+            index = index + 1
+
+        if ( not gotCharacter ) :
+            print("Keep trying")
 
     print("\n")
     print("Ending Hangman.")
