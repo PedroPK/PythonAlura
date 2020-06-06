00c65f076c33ba731b847ea22d95cf52e5905e2e
pedropk <pedropk@gmail.com>
2020-06-06 14:03:02 -0300

    Class 03.06 = Finding a character per time

--- AluraGames/hangman.py
+++ AluraGames/hangman.py
@@ -4,22 +4,50 @@ def play():
     print("#" * 30)
     print("\n")
 
-    secretWord = "banana"
+    # This is the Word that the User should try to discover
+    secretWord      = "banana"
+
+    # This will produce a String like this "_ _ _ _ _ _"
+    knownWord       = ("_ " * len(secretWord)).strip()
+    gotCharacters   = [None] * len(secretWord)
+    #print(knownWord)
 
     isHanged        = False
     gotWord         = False 
 
     while ( not isHanged and not gotWord ) :
         guess   =   input("Type a character: ")
-        guess   =   guess.strip()               # Java String.trim()
+        guess   =   guess.strip()           # Java String.trim()
         gotCharacter    = False
 
         index = 0
         for character in secretWord :
             if ( character.lower() == guess.lower() ) :
                 print("You got one. The character {} exists in position {}".format(character, index))
-                gotCharacter = True
+                gotCharacter            = True
+
+                # This will fill the List of Known Characters
+                gotCharacters[index]    =   character
+                print(gotCharacters)
+
+                if ( index == 0 ) :
+                    secretWord          =   "_"     +   secretWord[index + 1: len(secretWord)]
+                elif ( index == len(secretWord) ) :
+                    secretWord          =   secretWord[0: len(secretWord) - 1]  + "_"
+                else :
+                    secretWord          =   secretWord[0: index] + "_" + secretWord[index + 1: len(secretWord)]
+
+                """
+                if ( index > 0 ) :
+                    knownWord = knownWord[0:index] + " " + character
+                    if ( index < len(knownWord) ) :
+                        knownWord = knownWord + 
+                """
+
                 break
+            
+            if (  ) :
+
             index = index + 1
 
         if ( not gotCharacter ) :
