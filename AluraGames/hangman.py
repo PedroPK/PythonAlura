4a3f620fdbd85c39bea8fb25f41a991920c7c4b6
pedropk <pedropk@gmail.com>
2020-06-07 15:19:58 -0300

    Changing FOR for WHILE for  to jump Indexes

--- AluraGames/hangman.py
+++ AluraGames/hangman.py
@@ -11,7 +11,7 @@ def play():
     secretWordList  =  stringUtils.string_to_list("banana")
 
     # This will produce a String like this "_ _ _ _ _ _"
-    knownWord       = ("_ " * len(secretWordList)).strip()
+    # knownWord       = ("_ " * len(secretWordList)).strip()
 
     # This List will store the Char that were discovered. 
     # Ex: In banana word, when user press 'a', it will be like ['_', 'a', '_', 'a', '_', 'a']
@@ -21,16 +21,16 @@ def play():
     gotWord         = False 
 
     while ( not isHanged and not gotWord ) :
-        guess   =   input("Type a character: ")
-        guess   =   guess.strip()           # Java String.trim()
-        gotCharacter    = False
+        guessChar   =   input("Type a character: ")
+        guessChar   =   guessChar.strip()    # Java String.trim()
+        #gotChar     = False
 
         # Convert the SecretWord into a List
         # The variable below were replaced by secretWordList
         #listSecretWord = stringUtils.string_to_list(secretWord)
 
         # Count the number of times that a Char exists in the String/List SecretWord
-        qtOccurrences = secretWordList.count(guess)
+        qtOccurrences = secretWordList.count(guessChar)
         #print("Count of Occurrences of the Character in the Secret Word: " + str(listSecretWord.count(character)))
 
         # Create a List of this Length
@@ -38,16 +38,22 @@ def play():
         indexListOfPositionsWhereCharWasFound   =   0
 
         index = 0
-        for character in secretWordList :
+        
+        while index < len(secretWordList) :
 
             # Traverse the SecretWord, finding positions of the Char
 
             # Let's use the List .index() Function to make the algorithm more performatic, jumping unecessary iterations
-
+            
+            if ( guessChar in secretWordList ) :
+                index = secretWordList.index(guessChar)
+            else :
+                print("Keep trying")
+            
             # If the .index() return -1, we break the Iteration
             # Else, we can jump to this index, maybe using Continue and Comparison from Index variable with .index() result
 
-            if ( character.lower() == guess.lower() ) :
+            if ( secretWordList[index].lower() == guessChar.lower() ) :
                 # Add this Positions in a List
                 listOfPositionsWhereCharWasFound[indexListOfPositionsWhereCharWasFound] = index
 
@@ -55,19 +61,18 @@ def play():
                 indexListOfPositionsWhereCharWasFound = indexListOfPositionsWhereCharWasFound + 1
 
                 # Commenting line below to print it only once, outside the FOR
-                #print("You got one. The character {} exists in position {}".format(character, index))
                 gotCharacter            = True
 
                 # This will fill the List of Known Characters
-                gotCharacters[index]    =   character
+                gotCharacters[index]    =   secretWordList[index]
 
                 if ( "_" not in gotCharacters ) :
                     gotWord     =   True
                     print("You got it!")
                     break
 
-                if ( index == 0 ) :
-                    secretWordList[index]    =   "_"    #     +   secretWordList[index + 1: len(secretWordList)]
+                #if ( index == 0 ) :
+                secretWordList[index]    =   "_"    #     +   secretWordList[index + 1: len(secretWordList)]
 
                 # Removing this Break makes it to find All Occurrences of a Character in the Word
                 #break
@@ -80,12 +85,9 @@ def play():
         positionsString = stringUtils.list_to_string_with_commas(listOfPositionsWhereCharWasFound)
 
         # Print the Message informing player all the positions the Char was found
-        print("You got one. The character {} exists in position(s) {}".format(character, positionsString))
+        print("You got one. The character {} exists in position(s) {}".format(guessChar, positionsString))
 
-        print("\n")
-
-        if ( not gotCharacter and not gotWord) :
-            print("Keep trying")
+        print("\n")            
 
     print("Ending Hangman.")
 
