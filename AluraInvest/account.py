26c70e29a2ae0010c31ebd522d7ca28aa6143980
pedropk <pedropk@gmail.com>
2020-06-13 13:22:03 -0300

    Class04.04 - {:3.2f} String Format() + Transfer()

--- AluraInvest/account.py
+++ AluraInvest/account.py
@@ -9,24 +9,30 @@ class Account :
     ) :
         print( "Accound created ... {}".format(self) )
 
-        self.number = number
-        self.owner_name = owner_name
-        self.balance = initial_value
-        self.debt_limit = debt_limit
+        self.__number       = number
+        self.__owner_name   = owner_name
+        self.__balance      = initial_value
+        self.__debt_limit   = debt_limit
 
     def statement(self) :
         print("This Account {} has US${}".format(
-                self.number, 
-                self.balance)
+                self.__number, 
+                self.__balance)
         )
 
     def deposit(self, value) : 
-        self.balance = self.balance + value
+        self.__balance = self.__balance + value
 
     def witdraw(self, value) :
-        self.balance = self.balance - value
+        self.__balance = self.__balance - value
+
+    def transfer(self, value, to_account) :
+        self.witdraw(value)
+        to_account.deposit(value)
+        print("Transfer from Account {} to Account {} with Value of ${:03.2f}".format(self.__number, to_account.__number, value))
 
 #if ( __name__ == "__main__" ) :
+'''
 conta = Account(123, "Pedro", 30.0, 500)
 conta.statement()
 
@@ -35,3 +41,15 @@ conta.statement()
 
 conta.witdraw(25)
 conta.statement()
+'''
+
+account_pedro   =   Account(123, "Pedro", 250.0, 500.0)
+account_jubs    =   Account(456, "Jubs",  750, 250)
+
+account_pedro.statement()
+account_jubs.statement()
+
+account_pedro.transfer(50, account_jubs)
+
+account_pedro.statement()
+account_jubs.statement()
