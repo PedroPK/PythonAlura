a47a7c41eaea8946ec03b892ffe3ffd8b6ea853b
pedropk <pedropk@gmail.com>
2020-06-14 13:04:11 -0300

    Course Python3 OO Advanced - Class 01.09.01
    Creating an empty Class Movie

---
+++ AluraCinema/movie.py
@@ -0,0 +1,12 @@
+class Movie :
+    pass
+
+    '''
+    def __init__(self, name, year, duration) :
+        self.name       = name
+        self.year       = year
+        self.duration   = duration
+    '''
+
+avengers = Movie()
+print(avengers)
