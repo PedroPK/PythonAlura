2f0ff8e9564e78c8e8e91d00b9615f209c8c6b1a
pedropk <pedropk@gmail.com>
2020-06-14 13:14:40 -0300

    Course Python3 OO Advanced - Class 01.09.02
    Init() constructor created

--- AluraCinema/movie.py
+++ AluraCinema/movie.py
@@ -1,12 +1,12 @@
 class Movie :
     pass
 
-    '''
+    
     def __init__(self, name, year, duration) :
         self.name       = name
         self.year       = year
         self.duration   = duration
-    '''
+    
 
-avengers = Movie()
+avengers = Movie("Avengers: Infinity Wars", 2018, 160 )
 print(avengers)
